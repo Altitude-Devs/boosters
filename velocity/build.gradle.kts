55d055478eb9aae8f8a674787e6e73d46814f828
Len <40720638+destro174@users.noreply.github.com>
2022-09-09 20:47:53 +0200

    Fix fuckup in build.gradle.kts

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
         exclude("com.alttd:Galaxy-API:1.19.2-R0.1-SNAPSHOT")
     }
     // Velocity
+    compileOnly("com.velocitypowered:velocity-api:3.1.2-SNAPSHOT") // Velocity
     annotationProcessor("com.velocitypowered:velocity-api:3.1.2-SNAPSHOT")
     // DiscordLink
     compileOnly("com.alttd.proxydiscordlink:ProxyDiscordLink:1.0.0-BETA-SNAPSHOT") {
@@ -23,7 +24,6 @@ dependencies {
 //    }
     //Luckperms
     compileOnly("net.luckperms:api:5.3")
-    compileOnly("com.velocitypowered:velocity-api:3.1.2-SNAPSHOT") // Velocity
 }
 
 tasks {

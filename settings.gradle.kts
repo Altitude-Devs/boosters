720226486c40611db938e95a05c7be880812fbe6
Len <40720638+destro174@users.noreply.github.com>
2022-09-09 20:52:16 +0200

    Update velocity repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,7 @@ dependencyResolutionManagement {
         mavenCentral()
         maven("https://repo.destro.xyz/snapshots") // Altitude - Galaxy
         maven("https://oss.sonatype.org/content/groups/public/") // Adventure
-        maven("https://nexus.velocitypowered.com/repository/maven-public/") // Velocity
+        maven("https://papermc.io/repo/repository/maven-public/") // Velocity
         maven("https://repo.spongepowered.org/maven") // Configurate
         maven("https://nexus.neetgames.com/repository/maven-releases/") // mcMMO
         maven("https://maven.enginehub.org/repo/") // worldguard
